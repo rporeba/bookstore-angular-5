71c50bdcd5f8191457001624b6194810db496e19
rporeba <rafal.poreba@altkom.pl>
2017-11-14 08:26:33 +0100

    bootstrap

---
+++ bookstore-angular-app/src/app/book/book-form/book-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-book-form',
+  templateUrl: './book-form.component.html',
+  styleUrls: ['./book-form.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BookFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- bookstore-angular-app/src/app/book/book.module.ts
+++ bookstore-angular-app/src/app/book/book.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
 import { BookListComponent } from './book-list/book-list.component';
+import { BookFormComponent } from './book-form/book-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BookRoutingModule
   ],
-  declarations: [BookListComponent]
+  declarations: [BookListComponent, BookFormComponent]
 })
 export class BookModule { }
